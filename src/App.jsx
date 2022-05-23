554b1b5a00440139b70b7419f418e7c63131ca4d
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-23 07:22:28 +0200

    react router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.scss';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageBooks } from './pages/PageBooks';
+import { NavLink, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Info Site</h1>
-			<PageWelcome />
-			<PageBooks />
+			<NavLink to="/welcome">Welcome</NavLink> |{' '}
+			<NavLink to="/books">Books</NavLink>
+			<Routes>
+				<Route path="/welcome" element={<PageWelcome />} />
+				<Route path="/books" element={<PageBooks />} />
+			</Routes>
 		</div>
 	);
 }
