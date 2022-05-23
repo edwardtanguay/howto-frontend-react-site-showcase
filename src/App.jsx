8f139c1f081f9d96c1979d6a6c475ef2e5f57b54
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-23 06:44:26 +0200

    witih photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.scss'
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Info Site</h1>
-    <p>Welcome to this site.</p>
-    </div>
-  )
+		return (
+				<div className="App">
+						<h1>Info Site</h1>
+						<p>Welcome to this site.</p>
+						<p>Here is the image:</p>
+						<hr />
+						<img src="src/images/bashCookbook.jpg" />
+						<hr />
+				</div>
+		);
 }
 
-export default App
+export default App;
